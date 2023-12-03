4e3f337bf7774f3ba60b5d8cd821af9da91f42d9
Charles Giese <giesec@tcd.ie>
2023-12-03 22:27:35 +0100

    finished 2 and attempted 3

--- helper/src/lib.rs
+++ helper/src/lib.rs
@@ -1,6 +1,6 @@
 use std::fs::File;
 use std::path::Path;
-use std::io::{self, BufRead};
+use std::io::{self, BufRead, BufReader};
 
 pub fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
 where P: AsRef<Path>, {
@@ -8,3 +8,13 @@ where P: AsRef<Path>, {
     Ok(io::BufReader::new(file).lines())
 }
  
+
+pub fn count_lines<P>(filename: P) -> usize
+where P: AsRef<Path>, {
+    let input = File::open(filename).unwrap();
+    let buffered = BufReader::new(input);
+    let line_count = buffered.lines().count();
+
+    line_count
+}
+
